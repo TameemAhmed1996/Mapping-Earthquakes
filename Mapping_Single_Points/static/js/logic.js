db7b066ba872f8017b400d0482eee247c081791b
TameemAhmed1996 <80506578+TameemAhmed1996@users.noreply.github.com>
2022-09-13 20:33:23 -0400

    Add files via upload

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,39 @@
+console.log("working");
+
+// Creating The Map Object With A Center And Zoom Level.
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// Adding A Marker To The Map For Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+	color: "black",
+    fillColor: "lightyellow",
+    radius: 300
+ }).addTo(map);
+
+// First Tile Layer.
+// Street View.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Second Tile Layer.
+// Satellite View.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Third Tile Layer.
+// Dark View.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+
+// Gray Map Tile Layer.
+streets.addTo(map);
